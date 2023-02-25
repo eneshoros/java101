edef346eb77514c2f706530bd239badc08cc86b3
Enes Horos <eneshrs81@gmail.com>
2023-02-25 03:08:50 +0300

    Hesap makinesi programı yapıldı

--- HesapMakinesi.java
+++ HesapMakinesi.java
@@ -10,23 +10,27 @@ public class HesapMakinesi {
         System.out.println("1-Toplama\n2-Çıkarma\n3-Çarpma\n4-Bölme");
         System.out.print("Seçiminiz : ");
         secim=input.nextInt();
-        if(secim==1)
-            System.out.println("Sonuç : "+(sayi1+sayi2));
-        else if (secim==2)
-            System.out.println("Sonuç : "+(sayi1-sayi2));
-        else if (secim==3)
-            System.out.println("Sonuç : "+(sayi1*sayi2));
-        else if (secim==4){
-            if(sayi2==0)
-                System.out.println("Sayı 0'a bölünemez");
-            else if (sayi1<sayi2) {
-                System.out.println("Integer sayı bölmesinde ilk sayı büyük olmalıdır");
-            }
-            else
-                System.out.println("Sonuç : "+(sayi1/sayi2));
+        switch (secim){
+            case 1:
+                System.out.println("Sonuç : "+(sayi1+sayi2));
+                break;
+            case 2:
+                System.out.println("Sonuç : "+(sayi1-sayi2));
+                break;
+            case 3:
+                System.out.println("Sonuç : "+(sayi1*sayi2));
+                break;
+            case 4:
+                if(sayi2==0)
+                    System.out.println("Sayı 0'a bölünemez");
+                else if (sayi1<sayi2) {
+                    System.out.println("Integer sayı bölmesinde ilk sayı büyük olmalıdır");
+                }
+                else
+                    System.out.println("Sonuç : "+(sayi1/sayi2));
+                break;
+            default:
+                System.out.println("Geçerli seçim yapılmadı");
         }
-            
-        else
-            System.out.println("Geçerli seçim yapılmadı");
     }
 }
