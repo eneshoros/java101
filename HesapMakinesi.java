3e5113656295f27aa718903a5162137d816355fe
Enes Horos <eneshrs81@gmail.com>
2023-02-25 03:03:51 +0300

    Hesap makinesi programı yapıldı

---
+++ HesapMakinesi.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class HesapMakinesi {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int sayi1,sayi2,secim;
+        System.out.print("İlk sayıyı girin : ");
+        sayi1=input.nextInt();
+        System.out.print("İkinci sayıyı girin : ");
+        sayi2=input.nextInt();
+        System.out.println("1-Toplama\n2-Çıkarma\n3-Çarpma\n4-Bölme");
+        System.out.print("Seçiminiz : ");
+        secim=input.nextInt();
+        if(secim==1)
+            System.out.println("Sonuç : "+(sayi1+sayi2));
+        else if (secim==2)
+            System.out.println("Sonuç : "+(sayi1-sayi2));
+        else if (secim==3)
+            System.out.println("Sonuç : "+(sayi1*sayi2));
+        else if (secim==4){
+            if(sayi2==0)
+                System.out.println("Sayı 0'a bölünemez");
+            else if (sayi1<sayi2) {
+                System.out.println("Integer sayı bölmesinde ilk sayı büyük olmalıdır");
+            }
+            else
+                System.out.println("Sonuç : "+(sayi1/sayi2));
+        }
+            
+        else
+            System.out.println("Geçerli seçim yapılmadı");
+    }
+}
