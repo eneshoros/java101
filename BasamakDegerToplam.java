3c1460695528272f26216d86e9f600f873f820e2
Enes Horos <eneshrs81@gmail.com>
2023-02-26 01:43:37 +0300

    1-999 arası Armstrong Sayı bulma eklendi

--- BasamakDegerToplam.java
+++ BasamakDegerToplam.java
@@ -16,5 +16,28 @@ public class BasamakDegerToplam {
             sayi /= 10;
         }
         System.out.println("Toplam : " + toplam);
+
+        /* 1-999 arası Armstrong Sayıları Bulan Program
+        int sayi=999,geciciSayi=sayi,basamakSayisi=0,basamakDegeri,basamakUs,toplam;
+
+        while(geciciSayi!=0){
+            geciciSayi/=10;
+            basamakSayisi++;
+        }
+        for (int i=999;i>=0;i--){
+            toplam=0;
+            geciciSayi=i;
+            while (geciciSayi!=0){
+                basamakDegeri=geciciSayi%10;
+                basamakUs=1;
+                for (int j=1;j<=basamakSayisi;j++)
+                    basamakUs*=basamakDegeri;
+                toplam+=basamakUs;
+                geciciSayi/=10;
+            }
+            if(toplam==i)
+                System.out.println(toplam+" Armstrong Sayıdır");
+        }*/
+
     }
 }
